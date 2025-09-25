6ff0b1c5ee8b714ce39007fda1f5a14e76796b9c
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-09-25 09:33:16 -0400

    first commit.

---
+++ scripts/env_install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=env_install                        # Job name
+#SBATCH --output env_install.log                # Output log file
+#SBATCH --mail-type=NONE                            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=<USER>@gmail.com               # Email address to send status updates to
+#SBATCH --partition day                           # Train on private partition
+#SBATCH --requeue
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1                        # Run on a single CPU
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=10gb                                 # Job memory request
+#SBATCH --time=05:00:00                          # Time limit hrs:min:sec
+date;hostname;pwd
+
+PREFIX="/home/mr2238/project_pi_np442/mr2238/conda_envs/cppopt-dl"
+
+module reset
+
+module load miniconda
+cd /home/mr2238/cppopt-dl
+
+# this is just brute force env install because getting conda to work with .yml is terrible
+mamba create --prefix "$PREFIX" \
+    python=3.10 \
+    pip \
+    numpy \
+    pandas \
+    matplotlib \
+    tqdm \
+    h5py
+conda activate "$PREFIX"
+
+# install additional packages
+pip3 install torch torchvision torchaudio
+pip install accelerate datasets transformers
+pip install wandb
+
+conda deactivate
+
+# to delete the environment, run:
+# conda remove -n cppopt-dl --all
