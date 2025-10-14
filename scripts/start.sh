57c97843e07aaf4d5472071aaed7739b810d6a4e
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-10-14 14:58:16 -0400

    working start, really good now. Could consider adding automatic job canceling at end of script if desired.

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,16 +13,28 @@ TIME_FORMAT=$(printf "%02d:00:00" "$HOURS")
 
 cd ~
 
+echo "Starting vscode job and interactive job:"
+
 sed -i "s/^#SBATCH -t .*/#SBATCH -t $TIME_FORMAT/" "$TASK_FILE"
 
 sbatch "$TASK_FILE"
 
-salloc -t "$TIME_FORMAT" --mem-per-cpu=12G --cpus-per-task=4 srun --pty bash -c '
-    module load miniconda
-    source $(conda info --base)/etc/profile.d/conda.sh
-    conda activate cppopt-dl
-    cd ./accelerate
 
-    # Hand control to you without re-reading ~/.bashrc (so PS1 stays intact)
-    exec bash -i
-'
+# add temporary section to .bashrc
+BASHRC="$HOME/.bashrc"
+TEMP_TAG="# === TEMP SECTION ADDED BY SCRIPT ==="
+
+cd accelerate
+
+# Add temporary lines
+{
+     echo "$TEMP_TAG"
+     echo "module load miniconda; conda activate cppopt-dl"
+     echo "$TEMP_TAG"
+} >> "$BASHRC"
+
+
+salloc -t "$TIME_FORMAT" --mem-per-cpu=12G --cpus-per-task=4
+
+sed -i "/$TEMP_TAG/,/$TEMP_TAG/d" "$BASHRC"
+echo "Cleaned up .bashrc."
