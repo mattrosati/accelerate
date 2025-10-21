02fde3346b0bd6281b34cebea49146f97a01cd31
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-10-21 11:10:25 -0400

    Script to remake dataset quickly.

---
+++ scripts/remake_processed.sh
@@ -0,0 +1,3 @@
+
+
+python src/dataset_building.py ~/project_pi_np442/mr2238/accelerate/data/processed/ ~/project_pi_np442/mr2238/accelerate/data/raw_data/ ~/project_pi_np442/mr2238/accelerate/data/labels/
