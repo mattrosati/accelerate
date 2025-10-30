8eccf00fa4a6aae9002ddfb9ae74cebfd47c496c
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-10-30 12:08:18 -0400

    Data split into train and test by variable. Working on merging to match variable with smallest subset of windows.

--- src/data_extract.py
+++ src/data_extract.py
@@ -11,16 +11,33 @@ import pandas as pd
 import seaborn as sns
 
 from tqdm.contrib.concurrent import process_map
+from tqdm import tqdm
 
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
-from filelock import FileLock
+
 import zarr
+import dask.array as da
 
 from data_utils import build_continuous_time, load_label
 from constants import *
 from process_utils import get_window
 from outside_target import get_window_index, extract_proportions
+
+
+def merge_vars(variables, save_dir):
+    print(variables)
+    for split in ["train", "test"]:
+        for v in variables:
+            labels = pd.read_pickle(
+                os.path.join(save_dir, "temp", f"{split}_{v}_labels.pkl")
+            )
+            print(labels.shape)
+
+    # need to do an inner merge on DateTime for all variables on labels, but KEEP the indeces
+    # then subset the data arrays to match the indeces of the merged labels
+
+    return None
 
 
 def get_windows_var(v, ptid, file_path, window_index, window_s, strategy, percentage):
@@ -112,7 +129,6 @@ def extract_data(ptid, v, file_path, temp_dir_path, window_size, mode="mean"):
     Returns:
         None
     """
-
     window_index, window_s = (
         get_window_index(mode, window_seconds=window_size),
         window_size,
@@ -132,21 +148,47 @@ def extract_data(ptid, v, file_path, temp_dir_path, window_size, mode="mean"):
     return None
 
 
-def finalize(ptids, v, temp_dir_path, norm_dir):
+def finalize(v, split_dict, save_dir, ptids, debug=False):
     """
-    Finalize the extracted data for a given patient ID and variable by normalizing
-    and imputing missing values, then saving the final dataset.
+    Finalize the extracted data for a given variable, split into train and test.
 
     Args:
-        ptids (str): Patient ID.
         v (str): Variable to finalize.
+        split_dict (dict): Dictionary with keys being splits and values being list of ptids.
         temp_dir_path (str): Path to the temporary directory where intermediate results are stored.
         norm_dir (str): Path to the directory for saving normalization parameters.
     Returns:
         None
     """
+    # build separate train and test dask arrays
+    train_dir = os.path.join(save_dir, "train")
+    test_dir = os.path.join(save_dir, "test")
 
-    # TODO: needs to be made
+    for s, ptids in split_dict.items():
+        zarr_all_store = os.path.join(save_dir, "temp", f"{s}_{v}_x.zarr")
+        labels_all_store = os.path.join(save_dir, "temp", f"{s}_{v}_labels.pkl")
+
+        # go through ptids and append to cumulative var arrays
+        print(f"Finalizing {s}:")
+        for i, p in tqdm(enumerate(ptids), total=len(ptids)):
+            zarr_pt_store = os.path.join(save_dir, "temp", v, f"{p}_x.zarr")
+            labels_pt_store = os.path.join(save_dir, "temp", v, f"{p}_labels.pkl")
+            if i == 0:
+                z_arr = da.from_zarr(zarr_pt_store)
+                base = z_arr
+                labels_df = pd.read_pickle(labels_pt_store)
+            else:
+                base = da.concatenate([base, da.from_zarr(zarr_pt_store)], axis=0)
+                labels_df = pd.concat(
+                    [labels_df, pd.read_pickle(labels_pt_store)],
+                    axis=0,
+                ).reset_index(drop=True)
+
+        da.to_zarr(base, url=zarr_all_store, overwrite=True)
+        labels_df.to_pickle(labels_all_store)
+
+    if not debug:
+        shutil.rmtree(os.path.join(save_dir, "temp", v))
 
     return None
 
@@ -195,40 +237,56 @@ if __name__ == "__main__":
 
     # make test, train and temp directories, prepare for saving
     save_dir = os.path.join(args.save_dir, dataset_name)
+    # overwrite save dir if it exists
+    if os.path.exists(save_dir):
+        print("Overwriting existing save directory.")
+        shutil.rmtree(save_dir)
+
+    # make new dirs
     for split in ["train", "test", "temp", "scalers"]:
-        os.makedirs(os.path.join(save_dir, split), exist_ok=True)
+        os.makedirs(os.path.join(save_dir, split))
 
     temp_dir = os.path.join(save_dir, "temp")
     norm_dir = os.path.join(save_dir, "scalers")
-    # need healthy ptids
+    split_dict = {"train": [], "test": []}
+
+    # need healthy ptids and dict of which split they fall in
     with h5py.File(args.data_file, "r") as f:
         ptids = f["healthy_ptids"][:].astype(str).tolist()
+        # for debugging
+        ptids = ptids[:5]
+        for p in ptids:
+            if f[p].attrs["split"] == "train":
+                split_dict["train"].append(p)
+            else:
+                split_dict["test"].append(p)
 
     # will do everything and write to file in temp_dir
     for var in args.variables:
         print(f"Extracting for variable {var}:")
+        os.makedirs(os.path.join(temp_dir, var))
         func = partial(
             extract_data,
             v=var,
             file_path=args.data_file,
-            temp_dir_path=temp_dir,
+            temp_dir_path=os.path.join(temp_dir, var),
             window_size=args.window_size,
         )
         results = process_map(func, ptids, max_workers=os.cpu_count(), chunksize=1)
 
         print("Merging into final datasets (with normalization and imputation):")
-        func = partial(
-            finalize,
-            v=var,
-            temp_dir_path=temp_dir,
-            norm_dir=norm_dir,
-        )
-        results = process_map(func, ptids, max_workers=os.cpu_count(), chunksize=1)
+        finalize(var, split_dict, save_dir, ptids, debug=args.debug)
 
         print("")
 
-    # take finalized data from temp_dir, merge, and move to save_dir
+    # check labels
+    merge_vars(args.variables, save_dir)
+
+    # preprocess dataset
+    # pipeline()
+
+    # take finalized data from temp_dir, concatenate across vars, and move to save_dir
 
     # delete temp_dir
-    if not args.debug:
-        shutil.rmtree(temp_dir)
+    # if not args.debug:
+    #     shutil.rmtree(temp_dir)
