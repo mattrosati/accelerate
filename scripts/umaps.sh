f3d446b53a426e8c8e3e628972134be7d71d1c7a
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-10-31 12:05:17 -0400

    Better independent umapping scripts.

--- scripts/umaps.sh
+++ scripts/umaps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=accelerate-umap
-#SBATCH --output=logs/umap_%J.log
+#SBATCH --output=logs/output_umap_%A_%a.out
+#SBATCH --error=logs/output_umap_%A_%a.out
+#SBATCH --array=0-1   # <-- Two jobs: task IDs 0 and 1
 #SBATCH --partition day                           # Train on day
 #SBATCH --requeue
 #SBATCH --mem-per-cpu=20G 
@@ -8,14 +10,26 @@
 #SBATCH --time=1-00:00:00                         # Time limit hrs:min:sec
 
 date;hostname;pwd
+echo "Running task ID: ${SLURM_ARRAY_TASK_ID}"
 
 module load miniconda
 conda activate cppopt-dl
 
 cd /home/mr2238/accelerate
 
-# Do umaps
-echo "Calculating all umaps for plots and etc."
-python -u src/umaps.py
+if [ "${SLURM_ARRAY_TASK_ID}" -eq 1 ]; then
+    echo "Delaying start of job 1 by 20 minutes..."
+    sleep 20m
+fi
+
+if [ "${SLURM_ARRAY_TASK_ID}" -eq 1 ]; then
+    echo "Calculating umaps on raw windows."
+    python -u src/umaps.py --do_big
+elif [ "${SLURM_ARRAY_TASK_ID}" -eq 0 ]; then
+    echo "Calculating umaps on PCA of windows."
+    python -u src/umaps.py
+fi
 
 conda deactivate
+
+
--- src/umaps.py
+++ src/umaps.py
@@ -2,6 +2,7 @@ import os
 import sys
 import random
 import pickle
+from argparse import ArgumentParser
 
 import h5py
 
@@ -18,6 +19,7 @@ from tqdm import tqdm
 
 from data_utils import *
 from constants import *
+from process_utils import impute
 
 # pending useful sklearn imports
 from scipy.stats import pearsonr
@@ -30,11 +32,14 @@ from dask_ml.impute import SimpleImputer
 from dask_ml.decomposition import PCA
 
 if __name__ == "__main__":
+    
+    parser = ArgumentParser()
+    parser.add_argument('--do_big', action='store_true', help='Whether to do the big umap or the pca+umap')
+    args = parser.parse_args()
+
     pd.options.display.float_format = "{:10,.2f}".format
-    plt.rcParams.update({"xtick.bottom": True, "ytick.left": True})
     np.random.seed(420)
     random.seed(420)
-    sns.set_theme(context="talk")
     # path constants
     data_dir = "/home/mr2238/project_pi_np442/mr2238/accelerate/data"
     img_dir = "/home/mr2238/project_pi_np442/mr2238/accelerate/imgs/in_out"
@@ -43,14 +48,14 @@ if __name__ == "__main__":
     )
     labels_path = os.path.join(data_dir, "labels")
     raw_data_path = os.path.join(data_dir, "raw_data")
-    # check img directory exists, if not make it
-    os.makedirs(img_dir, exist_ok=True)
 
     # make combined info df
     information = []
     mode = "mean"
     with h5py.File(global_path, "r") as f:
         for pt in np.array(f["healthy_ptids"][...]).astype(str):
+            if f[f"{pt}/processed/in_out_{mode}"].attrs["in_out_broken"]:
+                continue
             in_out_df = pd.Series(
                 f[f"{pt}/processed/in_out_{mode}/in_out"][...]
             ).astype(bool)
@@ -66,78 +71,87 @@ if __name__ == "__main__":
     info_df["len"] = info_df["endidx"] - info_df["startidx"]
     assert len(info_df["len"].unique()) == 1
 
+    os.makedirs("/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/", exist_ok=True)
     zarr_store = "/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/array.zarr"
-    wind_dask = da.from_zarr(zarr_store)
+    if not os.path.exists(zarr_store):
+        print("Making windows...")
+        # make windows zarr store
+        
+        big_arr_path = '/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/memmap.dat'
 
-    imputer = SimpleImputer(strategy="mean")
-    windows_imp = imputer.fit_transform(wind_dask[:, 1:])
+        # save all windows as zarr file 
+        # saving all windows into a numpy memmap file
+        windows = np.memmap(big_arr_path, dtype='float32', mode='w+', shape=[info_df.shape[0], info_df['len'][0] + 1])
+        with h5py.File(global_path, "r") as f:
+            nan_value = f.attrs["invalid_val"]
+            uniques = info_df.ptid.unique()
+            for pt in tqdm(uniques):
+                abp_arr = f[f"{pt}/raw/waves/abp"]
+                ptid_info = info_df[info_df['ptid'] == pt]
+                for w, row in ptid_info.iterrows():
+                    abp_arr.read_direct(windows, source_sel=np.s_[row['startidx']: row['endidx']], dest_sel=np.s_[w, 1:])
+                    windows[w, 0] = w
+                    windows[w, windows[w] == nan_value] = np.nan
+                    windows[w, 1:] = impute(windows[w, 1:], strategy='lin_interpolate')
+                # print(windows[w, :])
+
+        windows.flush()
+
+        # need to transfer windows memmap to zarr
+        zarr.save_array(zarr_store, windows, chunks=(5000, 7501))
+
+        del windows
+        os.remove(big_arr_path)
+
+
+    wind_dask = da.from_zarr(zarr_store)
 
     # manual scaling
     mean_all = da.nanmean(wind_dask[:, 1:]).compute()
     std_all = da.nanstd(wind_dask[:, 1:]).compute()
-    scaled_values = (windows_imp - mean_all) / std_all
+    scaled_values = (wind_dask - mean_all) / std_all
     scaled_values.compute()
 
     # do umap
-    # print("Doing large UMAP....")
-    # fit = umap.UMAP(verbose=True)
-    # u = fit.fit_transform(scaled_values)
-    # u_df = pd.DataFrame(u, columns=["dim_1", "dim_2"])
-    # index_w = wind_dask[:, 0].compute()
-    # u_df = u_df.set_index(index_w)
-    # info_df[["dim_1", "dim_2"]] = u_df
-    # info_df.to_pickle(
-    #     "/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/large_umap.pkl"
-    # )
-    # print("Done")
-    # del fit
+    if args.do_big:
+        print("Doing large UMAP....")
+        fit = umap.UMAP(verbose=True)
+        u = fit.fit_transform(scaled_values)
+        u_df = pd.DataFrame(u, columns=["dim_1", "dim_2"])
+        index_w = wind_dask[:, 0].compute()
+        u_df = u_df.set_index(index_w)
+        info_df[["dim_1", "dim_2"]] = u_df
+        info_df.to_pickle(
+            "/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/large_umap.pkl"
+        )
+        print("Done")
+        del fit
+    else:
+        print("Doing PCA")
+        n_dim = 600
+        pca = PCA(n_components=n_dim)
+        pca = pca.fit(scaled_values)
+        umap_dim = np.arange(n_dim)[pca.explained_variance_ratio_.cumsum() > 0.95][0] + 1
+        print(f"Using {umap_dim} dimensions for UMAP at threshold variance of 0.95.")
+        X = pca.transform(scaled_values)[:, :umap_dim]
+        print("Done.")
 
-    print("Doing PCA")
-    n_dim = 600
-    pca = PCA(n_components=n_dim)
-    pca = pca.fit(scaled_values)
-    umap_dim = np.arange(n_dim)[pca.explained_variance_ratio_.cumsum() > 0.95][0] + 1
-    X = pca.transform(scaled_values)[:, :umap_dim]
-    print("Done.")
+        print("Doing UMAP grid search...")
+        # umap and graph with changes in basic parameters
+        index_w = wind_dask[:, 0].compute()
+        for n_neighbors, min_dist in itertools.product([15, 50], [0.1, 0.25, 0.5]):
+            print(n_neighbors, min_dist)
+            fit2 = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, verbose=True)
+            u2 = fit2.fit_transform(X)
+            u2_df = pd.DataFrame(u2, columns=["dim_1b", "dim_2b"])
+            u2_df = u2_df.set_index(index_w)
+            # combine datasets:
+            info_df[
+                [f"dim_1_{n_neighbors}_{min_dist}", f"dim_2_{n_neighbors}_{min_dist}"]
+            ] = u2_df
 
-    print("Doing UMAP grid search...")
-    # umap and graph with changes in basic parameters
-    index_w = wind_dask[:, 0].compute()
-    for n_neighbors, min_dist in itertools.product([15, 50], [0.1, 0.25, 0.5]):
-        print(n_neighbors, min_dist)
-        fit2 = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, verbose=True)
-        u2 = fit2.fit_transform(X)
-        u2_df = pd.DataFrame(u2, columns=["dim_1b", "dim_2b"])
-        u2_df = u2_df.set_index(index_w)
-        # combine datasets:
-        info_df[
-            [f"dim_1_{n_neighbors}_{min_dist}", f"dim_2_{n_neighbors}_{min_dist}"]
-        ] = u2_df
+        info_df.to_pickle(
+            f"/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/total_umaps2.pkl"
+        )
 
-        # # graph and color by in or out
-        # fig, ax = plt.subplots(figsize=(10, 10))
-        # sns.scatterplot(
-        #     data=info_df,
-        #     x=f"dim_1_{n_neighbors}_{min_dist}",
-        #     y=f"dim_2_{n_neighbors}_{min_dist}",
-        #     hue="in?",
-        #     palette="Set1",
-        #     s=1,
-        #     linewidth=0,
-        #     alpha=0.4,
-        # )
-        # ax.set_xlabel("UMAP Dimension 1")
-        # ax.set_ylabel("UMAP Dimension 2")
-        # plt.title("UMAP colored by label")
-        # leg = plt.legend(title="Inside AR Limits?", markerscale=10)
-        # for lh in leg.legend_handles:
-        #     lh.set_alpha(1)
-        # img_name = f"pca_umap_neigh{n_neighbors}_dist{min_dist}.png"
-        # plt.savefig(os.path.join(img_dir, img_name))
-        # plt.close()
-
-    info_df.to_pickle(
-        f"/home/mr2238/project_pi_np442/mr2238/accelerate/data/processed/windows/total_umaps2.pkl"
-    )
-
-    print("Done.")
+        print("Done.")
