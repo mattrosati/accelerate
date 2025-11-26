81bdaf1e679d5517e756b931d8609e5fa615b9fa
Matteo Rosati <matteo.rosati98@gmail.comu>
2025-11-26 09:47:44 -0500

    Raytune, optuna integration.

--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ import sys
 import shutil
 from argparse import ArgumentParser
 from pickle import dump
+from datetime import datetime
+import random
 
 import h5py
 
@@ -20,11 +22,24 @@ from functools import partial
 import zarr
 import dask.array as da
 
+from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import RandomizedSearchCV, RepeatedStratifiedKFold
+from sklearn.metrics import balanced_accuracy_score, auc
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from dask_ml.model_selection import HyperbandSearchCV
+
+from ray import tune
+
+import xgboost as xgb
 
 from data_utils import build_continuous_time, load_label
 from constants import *
 from process_utils import *
+from tuner import RayAdaptiveRepeatedCVSearch
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -33,60 +48,173 @@ if __name__ == "__main__":
         "--train_dir",
         help="Directory to training data and to store models.",
         default="/home/mr2238/project_pi_np442/mr2238/accelerate/data/training/w_300s_hr_rso2r_rso2l_spo2_abp",
-        required=True,
     )
     parser.add_argument(
         "--model",
         "-m",
         type=str,
         help="Type of model to train",
-        choices=["log_reg", "xgboost", "svm", "knn", "rand_forest"],
+        choices=["log_reg", "svm", "knn", "rand_forest", "decision_tree", "xgb"],
         required=True,
+    )
+    parser.add_argument(
+        "--data_mode",
+        type=str,
+        choices=['raw', 'pca', 'fpca', 'separate_pca'],
+        help="Transofrmations applied to mode prior to loading.",
+        default='raw',
+    )
+    parser.add_argument(
+        '-d',
+        '--debug',
+        action="store_true",
+        help="Trains on smaller subset of data. Saves models separately."
+    )
+    parser.add_argument(
+        '--run_name',
+        type=str,
+        default="",
+        help="Names run, defaults to timestamp if empty."
     )
 
     args = parser.parse_args()
     np.random.seed(420)
+    random.seed(420)
 
-    # make model saving dir
-    model_store = os.path.join(args.train_dir, "models")
+    print(f"Training model {args.model} with {args.data_mode} embeddings.")
+    
+    # decide what embeddings to use and load data
+    if args.data_mode == 'raw':
+        f = 'x.zarr'
+    elif args.data_mode == 'pca':
+        f = 'pca_x.zarr'
+    elif args.data_mode == 'fpca':
+        f = 'fpca_x.zarr'
+    elif args.data_mode == 'separate_pca':
+        f = 'separate_decomp_x.zarr'
+    X_train = da.from_zarr(os.path.join(args.train_dir, 'permanent', 'train', f))
+    labels = pd.read_pickle(os.path.join(args.train_dir, 'permanent', 'train', "labels.pkl"))
+    y_train = labels["in?"].astype(int)
+    groups = labels["ptid"].astype(str)
+
+    # shuffle and index if debugging
+    if args.debug:
+        print("Using a smaller dataset size to be speedy.")
+        length = 10_000
+        keys = da.random.random(X_train.shape[0], chunks=X_train.chunksize[0])
+        perm = np.argsort(keys)[:length]
+        X_train = X_train[perm]
+        y_train = y_train[perm]
+        groups = groups[perm]
+        print(f"Training dataset shape: {X_train.shape}.")
+
+    # make model saving dir, timestamped or run_name
+    if args.run_name == "":
+        run_name = datetime.now().strftime("%Y-%m-%d_%H:%M")
+    else:
+        run_name = args.run_name
+    models_dir_name = 'models_debug' if args.debug else "models"
+    models_dir_name = f"{models_dir_name}_{run_name}"
+    model_store = os.path.join(args.train_dir, models_dir_name)
     os.makedirs(model_store, exist_ok=True)
 
+    # init accuracies
+    metrics = {
+        'auc': 'roc_auc',
+        'balanced_accuracy': 'balanced_accuracy',
+    }
+
     # init model
+    n_iter = 30
     if args.model == "log_reg":
-        pass
-    elif args.model == "xgboost":
-        pass
-    elif args.model == "svm":
-        pass
-    elif args.model == "knn":
-        model = KNeighborsClassifier()
+        model = LogisticRegression(n_jobs=1, solver='saga', max_iter=10_000)
         params = {
-            "n_neighbors": np.linspace(1, 1_000, num=20).astype(int),
-            "weights": ["uniform", "distance"],
+            "penalty": tune.choice(["l2", "l1"]),
+            "C":  tune.loguniform(0.001, 100),
+        }
+    elif args.model == "decision_tree":
+        model = DecisionTreeClassifier()
+        params = {
+            'max_depth': tune.qrandint(5, 50, 5),
+            'max_features': tune.choice([0.02, 0.05, 0.1, 'sqrt']),
+            'min_samples_split': tune.randint(2, 25),
+            'min_samples_leaf': tune.randint(1, 15),
+            'class_weight': tune.choice([None, 'balanced']),
+        }
+        n_iter = len(list(params.keys())) * 10
+    elif args.model == "svm":
+        model = svm.SVC()
+        params = {
+            "C":  tune.loguniform(0.001, 100),
+            "gamma":  tune.loguniform(1/(X_train.shape[1]**3), 0.01),
+        }
+    elif args.model == "knn":
+        model = KNeighborsClassifier(n_jobs=1)
+        params = {
+            "n_neighbors": tune.lograndint(5, 1_000),
+            "weights": tune.choice(["uniform", "distance"]),
         }
     elif args.model == "rand_forest":
-        pass
+        model = RandomForestClassifier()
+        params = {
+            'n_estimators': tune.lograndint(50, 0.05 * X_train.shape[0]),
+            'max_depth': tune.qrandint(5, 50, 5),
+            'max_features': tune.choice([0.02, 0.05, 0.1, 'sqrt']),
+            'min_samples_split': tune.randint(2, 25),
+            'min_samples_leaf': tune.randint(1, 15),
+            'class_weight': tune.choice([None, 'balanced']),
+        }
+        n_iter = (len(list(params.keys())) - 1) * 10
+    elif args.model == "xgb":
+        model = xgb.XGBClassifier(tree_method="hist", eval_metric='logloss')
+        params = {
+            'n_estimators': tune.lograndint(50, 0.05 * X_train.shape[0]),
+            'max_depth': tune.randint(2, 10),
+            'learning_rate': tune.loguniform(1e-5, 1e-1),
+            'subsample': tune.quniform(0.25, 0.75, 0.05),
+            'colsample_bytree': tune.quniform(0.5, 1.0, 0.05),
+            'gamma': tune.quniform(0, 5, 0.25),
+            'reg_alpha': tune.loguniform(1e-5, 20.0),
+            'reg_lambda': tune.loguniform(1e-5, 20.0),
+        }
+        n_iter = (len(list(params.keys())) - 2) * 10
 
-    # load data
 
-    # init random search
-    # n_examples = 4 * len(X_train)
-    n_params = 20
-    n_iters = n_params
-    search = HyperbandSearchCV(
-        model,
-        params,
-        max_iter=max_iter,
-        patience=True,
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=3) # repeats five folds 2 times
+    # search = RandomizedSearchCV(
+    #     model,
+    #     params,
+    #     scoring=metrics,
+    #     n_iter=n_iter,
+    #     refit='auc',
+    #     n_jobs=-1 if args.model != "xgb" else 1,
+    #     verbose=3,
+    #     cv=cv,
+    #     return_train_score=True,
+    # )
+    search = RayAdaptiveRepeatedCVSearch(
+        estimator=model,
+        search_space=params,
+        scoring=metrics,
+        num_samples=n_iter,
+        rank_metric='mean_val_auc',
+        cv=[5, 3],
+        store_path=model_store,
     )
+    X_train = X_train.compute()
 
     # train
-    search.fit(X_train, y_train)
-
-    # print best scores
+    search = search.fit(X_train, y_train, groups=groups)
 
     # save model
     dump(
         search.best_estimator_,
-        open(os.path.join(model_store, f"{args.model}.pkl"), "wb"),
+        open(os.path.join(model_store, f"{args.model}_{args.data_mode}.pkl"), "wb"),
     )
+
+    print("Search results:")
+    df = pd.DataFrame(search.cv_results_())
+    print(df)
+
+    # all I need to save are the results now
+    df.to_csv(os.path.join(model_store, f"{args.model}_{args.data_mode}_cv_results.csv"), index=False)
---
+++ src/tuner.py
@@ -0,0 +1,221 @@
+import os
+import numpy as np
+from sklearn.model_selection import RepeatedStratifiedKFold, StratifiedGroupKFold
+from sklearn.metrics import check_scoring
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler, HyperBandForBOHB
+from ray.tune.search.bohb import TuneBOHB
+from ray.tune.search.optuna import OptunaSearch
+from ray.tune.search import ConcurrencyLimiter
+import optuna
+
+def train_cv(config, X, y, folds, estimator, scoring):
+
+    model = estimator.set_params(**config)
+    summary = {}
+    metrics = {}
+    if not isinstance(y, np.ndarray):
+        y = y.to_numpy()
+    # Loop over all repeated CV folds
+    for fold_idx, (train_idx, val_idx) in enumerate(folds):
+
+        X_tr, X_val = X[train_idx], X[val_idx]
+        y_tr, y_val = y[train_idx], y[val_idx]
+
+        model.fit(X_tr, y_tr)
+
+        scores_train = scoring(estimator, X_tr, y_tr)
+        scores_val = scoring(estimator, X_val, y_val)
+
+        if fold_idx == 0:
+            for key in scores_train.keys():
+                summary[f"train_{key}"] = [scores_train[key]]
+                summary[f"val_{key}"] = [scores_val[key]]
+
+                metrics[f"mean_val_{key}"] = float(np.mean([scores_val[key]]))
+                metrics[f"mean_train_{key}"] = float(np.mean([scores_train[key]]))
+
+                metrics[f"std_val_{key}"] = 0.0
+                metrics[f"std_train_{key}"] = 0.0 
+        else:
+            for key in scores_train.keys():
+                summary[f"train_{key}"].append(scores_train[key])
+                summary[f"val_{key}"].append(scores_val[key])
+
+                metrics[f"mean_val_{key}"] = float(np.mean([scores_val[key]]))
+                metrics[f"mean_train_{key}"] = float(np.mean([scores_train[key]]))
+
+                metrics[f"std_val_{key}"] = float(np.std(summary[f"val_{key}"]))
+                metrics[f"std_train_{key}"] = float(np.std(summary[f"train_{key}"]))
+
+        
+        fold_summary = metrics | {"fold": fold_idx}
+
+        tune.report(metrics=fold_summary)
+
+
+class RayAdaptiveRepeatedCVSearch:
+    """
+    RayTune-based adaptive repeated cross-validation search.
+    Reproduces caret adaptive resampling behavior:
+        - repeated CV (default: 5x10)
+        - per-fold adaptive pruning
+        - minimum folds before pruning (grace_period)
+        - AUC + Balanced Accuracy evaluation
+        - StandardScaler preprocessing
+        - final best estimator refit on full data
+    """
+
+    def __init__(
+        self,
+        estimator,
+        search_space,
+        cv=None,
+        grace_period=6,
+        reduction_factor=2,
+        num_samples=50,
+        scoring='roc_auc',
+        rank_metric="mean_val_auc",
+        mode="max",
+        store_path="./ray_results",
+    ):
+        """
+        estimator: sklearn estimator class (e.g. RandomForestClassifier)
+        search_space: Ray Tune search space dict
+        cv: custom CV folds
+        grace_period: min CV folds before pruning (caret min=10)
+        reduction_factor: ASHA aggression
+        num_samples: number of hyperparameter trials
+        """
+        self.estimator = estimator
+        self.search_space = search_space
+        self.cv = cv if cv is not None else RepeatedStratifiedKFold(n_splits=5, n_repeats=3)
+        self.grace_period = grace_period
+        self.reduction_factor = reduction_factor
+        self.num_samples = num_samples
+        self.scoring = check_scoring(estimator, scoring=scoring)
+        self.mode = mode
+        self.rank_metric = rank_metric
+
+        self.folds = None
+        self.best_config = None
+        self.best_estimator_ = None
+        self.analysis = None
+        self.store_path = store_path
+
+        if isinstance(self.cv, list):
+            self.n_splits, self.n_repeats = self.cv[0], self.cv[1]
+
+    def _build_folds(self, X, y, groups=None):
+        if groups is None:
+            self.folds = list(self.cv.split(X, y))
+        else:
+            self.folds = list(self._make_repeated_stratified_group_kfold(X, y, groups))
+
+
+    def _make_repeated_stratified_group_kfold(self, X, y, groups, random_state=42):
+        n_splits = self.n_splits
+        n_repeats = self.n_repeats
+        
+        for r in range(n_repeats):
+            sgkf = StratifiedGroupKFold(
+                n_splits=n_splits,
+                shuffle=True,
+                random_state=random_state
+            )
+            for train_idx, val_idx in sgkf.split(X, y, groups):
+                yield train_idx, val_idx
+
+    
+    def fit(self, X, y, groups=None):
+
+        self._build_folds(X, y, groups=groups)
+
+        folds = self.folds
+        estimator = self.estimator
+
+        # -------------------------------
+        # Training function for Ray Tune
+        # -------------------------------
+
+        # -------------------------------
+        # Ray Tune scheduler (caret-like pruning)
+        # -------------------------------
+        scheduler = ASHAScheduler(
+            max_t=len(self.folds),
+            grace_period=self.grace_period,
+            reduction_factor=self.reduction_factor,
+        )
+
+        # -------------------------------
+        # Run Ray Tune
+        # -------------------------------
+        searcher = OptunaSearch(
+            sampler=optuna.samplers.TPESampler(
+                multivariate=True,        # better exploration
+                group=True,               # improves correlated parameters
+                n_startup_trials=10,
+                constant_liar=True,
+            ),
+        )
+        # searcher = ConcurrencyLimiter(searcher, max_concurrent=os.cpu_count())
+        trainable = tune.with_parameters(
+            train_cv,
+            X=X,
+            y=y,
+            folds=folds,
+            estimator=estimator,
+            scoring=self.scoring,
+        )
+        tuner = tune.Tuner(
+                    trainable,
+                    param_space=self.search_space,
+                    tune_config=tune.TuneConfig(
+                        metric=self.rank_metric,
+                        mode=self.mode,
+                        scheduler=scheduler,
+                        search_alg=searcher,
+                        num_samples=self.num_samples,
+                    ),
+                    run_config=tune.RunConfig(
+                        name="adaptive_repeated_cv_search",
+                        verbose=1,
+                        failure_config=tune.FailureConfig(fail_fast=False),
+                        storage_path=self.store_path,
+                    ),
+                )
+
+        self.results = tuner.fit()
+
+        # -------------------------------------------------------
+        # Extract best config
+        # -------------------------------------------------------
+        self.best_config = self.results.get_best_result(
+            metric=self.rank_metric,
+            mode=self.mode
+        ).config
+
+        # -------------------------------------------------------
+        # Fit final best estimator on full dataset
+        # -------------------------------------------------------
+        self.best_estimator_ = estimator.set_params(**self.best_config)
+        self.best_estimator_.fit(X, y)
+
+        return self
+
+    def predict(self, X):
+        return self.best_estimator_.predict(X)
+
+    def predict_proba(self, X):
+        if hasattr(self.best_estimator_, "predict_proba"):
+            return self.best_estimator_.predict_proba(X)[:, 1]
+        return self.best_estimator_.decision_function(X)
+
+    def get_best_config(self):
+        return self.best_config
+
+    def get_best_score(self):
+        return self.analysis.best_result.get(self.metric)
+
+    def cv_results_(self):
+        return self.results.get_dataframe().sort_values(by=self.rank_metric, ascending=(self.mode=="min"))
